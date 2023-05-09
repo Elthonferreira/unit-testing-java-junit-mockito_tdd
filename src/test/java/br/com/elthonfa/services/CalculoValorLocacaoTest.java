ad3ddbdabeda48addbc5146356ad60844f5e317c
Elthonferreira <elthonferreira18@gmail.com>
2023-05-09 00:11:57 -0300

    Correção nos valores esperados dos testes e adição de teste com 7 filmes sem desconto

--- src/test/java/br/com/elthonfa/services/CalculoValorLocacaoTest.java
+++ src/test/java/br/com/elthonfa/services/CalculoValorLocacaoTest.java
@@ -34,21 +34,23 @@ public class CalculoValorLocacaoTest {
         locacaoService = new LocacaoService();
     }
 
-    private static Filme filme1 = new Filme("Interestelar", 10, 100D);
-    private static Filme filme2 = new Filme("Ilha do medo", 10, 100D);
+    private static Filme filme1 = new Filme("Interstellar", 10, 100D);
+    private static Filme filme2 = new Filme("Shutter Island", 10, 100D);
     private static Filme filme3 = new Filme("Joker", 10, 100D);
     private static Filme filme4 = new Filme("Harry Potter", 10, 100D);
-    private static Filme filme5 = new Filme("O Predestinado", 10, 100D);
+    private static Filme filme5 = new Filme("Predestination", 10, 100D);
     private static Filme filme6 = new Filme("Seven", 10, 100D);
+    private static Filme filme7 = new Filme("Catch me if you can", 10, 100D);
 
     @Parameterized.Parameters(name = "{2}") // {2} é o terceiro parâmetro
     public static Collection<Object[]> getParametros() {
         return Arrays.asList(new Object[][] {
                 {Arrays.asList(filme1, filme2), 190D, "2 Filmes: deve conceder 10% de desconto no segundo filme"},
-                {Arrays.asList(filme1, filme2, filme3), 275D, "3 Filmes: deve conceder 25% de desconto no terceiro filme"},
-                {Arrays.asList(filme1, filme2, filme3, filme4), 325D, "4 Filmes: deve conceder 50% de desconto no quarto filme"},
-                {Arrays.asList(filme1, filme2, filme3, filme4, filme5), 350D, "5 Filmes: deve conceder 75% de desconto no quinto filme"},
-                {Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6), 350D, "6 Filmes: deve conceder 100% de desconto no sexto filme"},
+                {Arrays.asList(filme1, filme2, filme3), 265D, "3 Filmes: deve conceder 25% de desconto no terceiro filme"},
+                {Arrays.asList(filme1, filme2, filme3, filme4), 315D, "4 Filmes: deve conceder 50% de desconto no quarto filme"},
+                {Arrays.asList(filme1, filme2, filme3, filme4, filme5), 340D, "5 Filmes: deve conceder 75% de desconto no quinto filme"},
+                {Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6), 340D, "6 Filmes: deve conceder 100% de desconto no sexto filme"},
+                {Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6, filme7), 440D, "7 Filmes: Sem descontos"},
         });
     }
 
